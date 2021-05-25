63142cee56d690c63f77017d742db034ab0c68d9
Arthur V Fain <arthurvfain@gmail.com>
2021-05-25 15:50:08 -0400

    passing to Kyle

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+getImage(imgUrl)
+
+let imageDump = document.querySelector('#dog-image-container')
+
+function getImage(url)
+{
+    fetch(url).then(resp => resp.json()).then(dog => appendDogs(dog.message))
+}
+
+function appendDogs(dogArray)
+{
+    dogArray.forEach(appendMethod)
+}
+
+function appendMethod(element)
+{
+    let dogPic = document.createElement('img')
+    dogPic.src = element
+    imageDump.appendChild(dogPic) 
+}
